5388fef843b57ac8cee353bffce70824bbb4f01d
satackey <21271711+satackey@users.noreply.github.com>
2020-02-24 23:13:16 +0900

    exit code

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,7 @@ mkdir -p ${DOC_DIR}/.kakomon-manager-legacy \
     && cp -r /app/* ${DOC_DIR}/.kakomon-manager-legacy \
     && cd ${DOC_DIR}/.kakomon-manager-legacy \
     && eval "$@"
+EXIT_CODE=$?
 
 rm -rf ${DOC_DIR}/.kakomon-manager-legacy/*
+exit $EXIT_CODE
